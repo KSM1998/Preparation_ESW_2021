53b20938f6b3f95700fdeb821dd26c1d5c7dd94c
KSM1998 <76582608+KSM1998@users.noreply.github.com>
2021-03-19 01:00:50 +0900

    branch를 위한 덮어쓰기(의미없는 커밋)

---
+++ OpenCV_4.5.1_Line_detection/TCP_test_Client.cpp
@@ -0,0 +1,39 @@
+//#include "stdafx.h"
+#include <stdio.h>
+#include<winsock.h>					//소켓을 사용하기 위한 헤더파일
+
+#pragma comment(lib, "ws2_32")		//위에서 선언한 헤더파일들을 가져다 쓰기위한 링크
+
+#define PORT 4578
+#define PACKET_SIZE 1024
+#define SERVER_IP "211.32.63.121"
+
+int main() 
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET hSocket;
+	hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tAddr = {};
+	tAddr.sin_family = AF_INET;
+	tAddr.sin_port = htons(PORT);
+	tAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+	connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+	char cMsg[] = "Client Send";
+	send(hSocket, cMsg, strlen(cMsg), 0);
+
+	char cBuffer[PACKET_SIZE] = {};
+	recv(hSocket, cBuffer, PACKET_SIZE, 0);
+
+	printf("RECV Msg : %s\n", cBuffer);
+
+	closesocket(hSocket);
+
+	WSACleanup();
+
+	return 0;
+}
